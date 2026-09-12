4d02c102e35e79afb7bd9c2528a52eb30021595e
Vishnu sivan <vishnusivan194@gmail.com>
2026-09-12 19:05:47 +0530

    Add files via upload

---
+++ Webdev-L1-LandingPage/script.js
@@ -0,0 +1,20 @@
+
+const navbar = document.getElementById("navbar");
+
+
+
+window.addEventListener("scroll", function () {
+
+    if (window.scrollY > 50) {
+
+        
+        navbar.classList.add("scrolled");
+
+    } else {
+
+        
+        navbar.classList.remove("scrolled");
+
+    }
+
+});
